60fd45fa79565e45ec4ef6bdbf445554c683bf28
makley <makley@o2.pl>
2017-11-24 20:32:50 +0000

    This is my second commit, just a simple comment

--- src/com/mwalas/Main.java
+++ src/com/mwalas/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //This is a test comment. I'm practicing with GIT!
         System.out.println("Hello, world!");
     }
 }
